5d2a1c06369fad23a58c0b1d944b4dd70df28428
Andres <carlos.camyoh@gmail.com>
2024-01-15 22:25:52 -0500

    Initial Commit

---
+++ GithubApp/GithubAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubAppApp.swift
+//  GithubApp
+//
+//  Created by Andres Mendieta on 15/01/24.
+//
+
+import SwiftUI
+
+@main
+struct GithubAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
